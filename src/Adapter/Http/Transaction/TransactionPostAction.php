c1cd2d91db4bdf954ad9eab5a85326a78c3a65cc
Lucas Dantas <lucas.dantas@voxtec.com.br>
2024-09-11 11:21:36 -0300

    feat: add service in endpoint

--- src/Adapter/Http/Transaction/TransactionPostAction.php
+++ src/Adapter/Http/Transaction/TransactionPostAction.php
@@ -2,17 +2,25 @@
 
 namespace App\Adapter\Http\Transaction;
 
+use App\Domain\Service\Transaction\TransactionServiceInterface;
 use App\Infrastructure\Dto\Transaction\CreateTransactionDTO;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
-class TransactionPostAction
+final readonly class TransactionPostAction
 {
+    public function __construct(
+        private TransactionServiceInterface $transactionService
+    ) {
+    }
+
     #[Route(path: "/api/v1/transaction", name: "api_transaction_post", methods: ["POST"], format: "json")]
     public function __invoke(#[MapRequestPayload(acceptFormat: "json")] CreateTransactionDTO $dto): JsonResponse
     {
+        $this->transactionService->createTransaction($dto);
+
         return new JsonResponse(null, Response::HTTP_CREATED);
     }
 }
