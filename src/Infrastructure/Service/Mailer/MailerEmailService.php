5cde2818eecfc232f7010b2d35a87719150b393c
Lucas Dantas <lucas.dantas@voxtec.com.br>
2024-09-12 11:50:52 -0300

    feat: add implementation of gateway to send email

---
+++ src/Infrastructure/Service/Mailer/MailerEmailService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Infrastructure\Service\Mailer;
+
+use App\Adapter\Email\EmailServiceGateway;
+use App\Domain\ValueObject\Email\EmailVO;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+readonly class MailerEmailService implements EmailServiceGateway
+{
+    public function __construct(
+      private MailerInterface $mailer
+    ) {
+    }
+
+    public function sendEmail(EmailVO $emailVO): void
+    {
+        $email = (new Email())
+            ->from($emailVO->getFrom())
+            ->to($emailVO->getTo())
+            ->subject("Transação realizada com sucesso!")
+            ->text("Você recebeu uma transação com sucesso!");
+
+        $this->mailer->send($email);
+    }
+}
