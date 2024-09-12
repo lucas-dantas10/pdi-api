1b67513ad149d7b7b1ca9cd9726c6276a18b72cc
Lucas Dantas <lucas.dantas@voxtec.com.br>
2024-09-12 17:29:23 -0300

    feat: add handler queue to send email

---
+++ src/Infrastructure/Service/Queue/EmailQueueHandler.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Infrastructure\Service\Queue;
+
+use App\Adapter\Email\EmailServiceGateway;
+use App\Domain\ValueObject\Email\EmailVO;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+readonly class EmailQueueHandler
+{
+    public function __construct(private EmailServiceGateway $emailService) {}
+
+    public function __invoke(EmailVO $emailVO)
+    {
+        $this->emailService->sendEmail($emailVO);
+    }
+}
