96da28d41af26e193a8030b8c9264efb3ee8b43f
Lucas Dantas <lucas.dantas@voxtec.com.br>
2024-09-12 19:58:45 -0300

    feat: add exception to transaction

--- src/Domain/Exception/Transaction/NotAuthorizedException.php
+++ src/Domain/Exception/Transaction/NotAuthorizedException.php
@@ -5,9 +5,9 @@ namespace App\Domain\Exception\Transaction;
 use Exception;
 use Throwable;
 
-class InsufficientBalanceException extends Exception
+class NotAuthorizedException extends Exception
 {
-    private const string MESSAGE = "Saldo insuficiente";
+    private const string MESSAGE = 'Transação não autorizada';
 
     public function __construct(?string $message = null, int $code = 0, ?Throwable $previous = null)
     {
