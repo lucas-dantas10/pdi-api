df9209c41de9c88e142ce92b3dc7bb5b62b52c6c
Lucas Dantas <lucas.dantas@voxtec.com.br>
2024-09-16 17:53:02 -0300

    config: add login config

--- src/Infrastructure/Service/TransactionAuthorizer/TransactionAuthorizerService.php
+++ src/Infrastructure/Service/TransactionAuthorizer/TransactionAuthorizerService.php
@@ -6,8 +6,10 @@ use App\Adapter\TransactionAuthorizer\TransactionAuthorizerGateway;
 
 class TransactionAuthorizer implements TransactionAuthorizerGateway
 {
+    private const string URL_BASE = '%env(resolve:TRANSACTION_AUTHORIZER_URL)%';
     public function authorize(): bool
     {
+        dd(self::URL_BASE);
         // TODO
         return true;
     }
