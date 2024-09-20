ecb1a54ab94a44e83698e1a3d78a18db2740ac81
Lucas Dantas <lucas.dantas@voxtec.com.br>
2024-09-20 17:20:32 -0300

    feat: add request to mock

--- src/Infrastructure/Service/TransactionAuthorizer/TransactionAuthorizerService.php
+++ src/Infrastructure/Service/TransactionAuthorizer/TransactionAuthorizerService.php
@@ -3,14 +3,38 @@
 namespace App\Infrastructure\Service\TransactionAuthorizer;
 
 use App\Adapter\TransactionAuthorizer\TransactionAuthorizerGateway;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 readonly class TransactionAuthorizerService implements TransactionAuthorizerGateway
 {
-    public function __construct(private string $baseUrl) {}
+    public function __construct(
+        private HttpClientInterface $client,
+        private string $baseUrl
+    ) {
+    }
 
     public function authorize(): bool
     {
-        // TODO
-        return true;
+        try {
+            $response = $this->client->request(
+                method: 'GET',
+                url: $this->baseUrl
+            );
+
+            $content = json_decode($response->getContent(), true);
+
+            return $content['status'];
+        } catch (
+            ClientExceptionInterface
+            | RedirectionExceptionInterface
+            | ServerExceptionInterface
+            | TransportExceptionInterface $e
+        ) {
+            return false;
+        }
     }
 }
