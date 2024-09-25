82b1ff113db62ffcca25730586e16324625e7861
Lucas Dantas <lucas.dantas@voxtec.com.br>
2024-09-25 18:00:57 -0300

    feat: add implementation to user repository

---
+++ src/Infrastructure/Repository/User/UserRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Infrastructure\Repository\User;
+
+use App\Domain\Entity\User;
+use App\Domain\Repository\User\CreateUserDTO;
+use App\Domain\Repository\User\UserRepositoryInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class UserRepository extends ServiceEntityRepository implements UserRepositoryInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    public function startTransaction(): void
+    {
+        $this->getEntityManager()->beginTransaction();
+    }
+
+    public function commitTransaction(): void
+    {
+        $this->getEntityManager()->commit();
+    }
+
+    public function rollbackTransaction(): void
+    {
+        $this->getEntityManager()->rollback();
+    }
+
+    public function persist(User $user): void
+    {
+        $this->getEntityManager()->persist($user);
+    }
+
+    public function save(): void
+    {
+        $this->getEntityManager()->flush();
+    }
+}
